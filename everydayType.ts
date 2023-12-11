86368cf94914adbb3dd5eb73621727521741ffa7
crystalchavez99 <chavezcrystal1999@yahoo.com>
2023-12-11 15:54:08 -0800

    interface

--- everydayType.ts
+++ everydayType.ts
@@ -90,3 +90,19 @@ function printCoord(pt: { x: number; y: number }) {
     console.log("The coordinate's y value is " + pt.y);
   }
   printCoord({ x: 3, y: 7 });
+
+/*
+Interfaces
+    An interface declaration is another way to name an object type:
+*/
+interface Point {
+    x: number;
+    y: number;
+  }
+   
+  function printCoordinate(pt: Point) {
+    console.log("The coordinate's x value is " + pt.x);
+    console.log("The coordinate's y value is " + pt.y);
+  }
+   
+  printCoord({ x: 100, y: 100 });
